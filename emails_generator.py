5121d04d7a4672c4376f543fb6e06e10f5d831fa
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-02-28 02:34:38 +0100

    Added readme and generated emails

--- emails_generator.py
+++ emails_generator.py
@@ -2,13 +2,9 @@ import random
 import json
 random_words = json.load(open('words.json'))
 random_domains = json.load(open('domains.json'))
-#generate 5 000 emails
-f = open('email_list_5000','w')
-for i in range(5000):
-    f.write(random_words[random.randint(0,len(random_words) - 1)] + '@' + random_domains[random.randint(0,len(random_domains) - 1)] + '\n')
-f.close()
-#generate 100 000 emails
-f = open('email_list_100000','w')
-for i in range(100000):
+n = int(input('Enter number of emails to generate'))
+name_of_file = input('Enter the name of file you want to save your emails to')
+f = open(name_of_file,'w')
+for i in range(n):
     f.write(random_words[random.randint(0,len(random_words) - 1)] + '@' + random_domains[random.randint(0,len(random_domains) - 1)] + '\n')
 f.close()
